1a32a940ee6a99c2426a8fc27273157e455ddd83
AfroTidus <AfroTidus@gmail.com>
2023-10-12 17:42:28 +0100

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 0; x<=100; x++)
+            {
+                if(x%3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+
+                if(x%5 == 0)
+                {
+                    Console.Write("Buzz");
+                }
+
+                else if(x % 3 != 0 && x % 5 != 0)
+                {
+                    Console.Write(x);
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
